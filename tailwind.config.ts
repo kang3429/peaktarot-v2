a3d8b6f200b5d409fd6c9dc1463aaa038acff5e4
kang3429 <kang3429@naver.com>
2025-04-16 20:50:09 +0900

    🔧 tailwind 애니메이션/스타일 적용 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,21 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+],
+theme: {
+  extend: {
+    animation: {
+      fadeIn: "fadeIn 1s ease-in-out",
+    },
+    keyframes: {
+      fadeIn: {
+        "0%": { opacity: "0" },
+        "100%": { opacity: "1" },
+      },
+    },
   },
-  plugins: [],
-};
+},
+plugins: [],
+}
 
-export default config;
+export default config
